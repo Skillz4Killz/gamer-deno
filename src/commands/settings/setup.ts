ce7cb42c5bfb76419c422153dea8252e9b024bcd
Skillz <gankstarsleague@gmail.com>
2020-09-01 23:14:21 -0400

    setup base

--- src/commands/settings/setup.ts
+++ src/commands/settings/setup.ts
@@ -3,6 +3,12 @@ import { botCache } from "../../../mod.ts";
 import { PermissionLevels } from "../../types/commands.ts";
 import { sendResponse } from "../../utils/helpers.ts";
 import { translate } from "../../utils/i18next.ts";
+import {
+  createGuildChannel,
+  deleteChannel,
+} from "https://x.nest.land/Discordeno@8.4.2/src/handlers/guild.ts";
+import { botID } from "https://x.nest.land/Discordeno@8.4.2/src/module/client.ts";
+import { Message } from "https://x.nest.land/Discordeno@8.4.2/src/structures/message.ts";
 
 const yesEmojiID = botCache.helpers.emojiID(botCache.constants.emojis.success);
 const quitEmojiID = botCache.helpers.emojiID(botCache.constants.emojis.quit);
@@ -11,6 +17,33 @@ const reactions = [
   botCache.constants.emojis.failure,
   botCache.constants.emojis.quit,
 ];
+
+function confirmedCancel(message: Message, channelID: string) {
+  sendResponse(
+    message,
+    translate(message.guildID, "commands/setup:CANCELLED"),
+  );
+
+  deleteChannel(message.guildID, channelID);
+}
+
+function cancelSetup(message: Message, responseMessage: Message) {
+  const CANCEL_OPTIONS = translate(
+    message.guildID,
+    "common:CANCEL_OPTIONS",
+    { returnObjects: true },
+  );
+
+  console.log(CANCEL_OPTIONS, responseMessage.content.toLowerCase());
+  if (
+    !CANCEL_OPTIONS.includes(responseMessage.content.toLowerCase())
+  ) {
+    return false;
+  }
+
+  confirmedCancel(message, responseMessage.channelID);
+  return true;
+}
 
 botCache.commands.set("setup", {
   name: "setup",
@@ -21,9 +54,60 @@ botCache.commands.set("setup", {
     seconds: 120,
     allowedUses: 2,
   },
-  execute: async function (message) {
-    // Thank the user for using Gamer!
-    sendMessage(message.channel, "commands/setup:BEGIN");
+  execute: async function (message, args, guild) {
+    if (!guild) return;
+
+    const mention = `<@!${message.author.id}>`;
+
+    sendResponse(
+      message,
+      translate(message.guildID, "commands/setup:PREPARING"),
+    );
+
+    // Create the setup spam channel
+    const setupChannel = await createGuildChannel(guild, "gamer-setup", {
+      position: 1,
+      permission_overwrites: [
+        {
+          id: botID,
+          allow: [
+            "VIEW_CHANNEL",
+            "SEND_MESSAGES",
+            "EMBED_LINKS",
+            "ADD_REACTIONS",
+            "READ_MESSAGE_HISTORY",
+            "MANAGE_CHANNELS",
+            "USE_EXTERNAL_EMOJIS",
+          ],
+          deny: [],
+          type: "member",
+        },
+        {
+          id: message.author.id,
+          allow: [
+            "VIEW_CHANNEL",
+            "SEND_MESSAGES",
+            "EMBED_LINKS",
+            "ADD_REACTIONS",
+            "READ_MESSAGE_HISTORY",
+            "MANAGE_CHANNELS",
+            "USE_EXTERNAL_EMOJIS",
+          ],
+          deny: [],
+          type: "member",
+        },
+      ],
+    });
+
+    // Thank the user for using Gamer! And get them into the setup channel
+    await sendMessage(
+      setupChannel,
+      translate(
+        message.guildID,
+        "commands/setup:BEGIN",
+        { mention },
+      ),
+    );
 
     const CANCEL_OPTIONS = translate(
       message.guildID,
@@ -32,40 +116,33 @@ botCache.commands.set("setup", {
     );
 
     // Ask first question.
-    const beginMessage = await sendResponse(
-      message,
-      translate(message.guildID, "commands/setup:SUBSCRIBE_QUESTION"),
+    const beginMessage = await sendMessage(
+      setupChannel,
+      translate(
+        message.guildID,
+        "commands/setup:SUBSCRIBE_QUESTION",
+        { mention },
+      ),
     );
-    await addReactions(message.channelID, beginMessage.id, reactions);
+    await addReactions(beginMessage.channelID, beginMessage.id, reactions);
     const subscribe = await botCache.helpers.needReaction(
       message.author.id,
       beginMessage.id,
     );
-    if (subscribe === quitEmojiID) return;
+    if (subscribe === quitEmojiID) {
+      return confirmedCancel(message, setupChannel.id);
+    }
     // The user wants to subscribe
     if (subscribe === yesEmojiID) {
-      sendResponse(
-        message,
-        translate(message.guildID, "commands/setup:NEED_CHANNEL"),
+      sendMessage(
+        setupChannel,
+        translate(message.guildID, "commands/setup:NEED_CHANNEL", { mention }),
       );
       const response = await botCache.helpers.needMessage(
         message.author.id,
-        message.id,
-        {
-          filter: (msg) =>
-            message.author.id === msg.author.id &&
-            (CANCEL_OPTIONS.includes(msg.content.toLowerCase()) ||
-              Boolean(msg.mentionChannels?.length)),
-        },
+        setupChannel.id,
       );
-      if (
-        !response || CANCEL_OPTIONS.includes(response.content)
-      ) {
-        return sendMessage(
-          message.channel,
-          translate(message.guildID, "commands/setup:CANCELLED"),
-        );
-      }
+      if (cancelSetup(message, response)) return;
 
       const [targetChannel] = response.mentionChannels;
       // Subscribe to gamer news channels
@@ -74,6 +151,25 @@ botCache.commands.set("setup", {
       }
     }
 
-    // Step 2:
+    // Step 2: Setup TODO Feature
+    const todoMessage = await sendMessage(
+      setupChannel,
+      translate(message.guildID, "commands/setup:TODO_SETUP", { mention }),
+    );
+    await addReactions(todoMessage.channelID, todoMessage.id, reactions);
+    const todo = await botCache.helpers.needReaction(
+      message.author.id,
+      todoMessage.id,
+    );
+    if (todo === quitEmojiID) return confirmedCancel(message, setupChannel.id);
+
+    // The user wants to setup todo feature
+    if (todo === yesEmojiID) {
+      await botCache.commands.get("todo")
+        ?.subcommands?.get("setup")
+        ?.execute?.(message, {}, guild);
+    }
+
+    // Step 3: Unique Role Set
   },
 });
