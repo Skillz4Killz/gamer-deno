b4cd5e68dfe038ff72820b85aec2f1b5fa97ad75
Skillz <gankstarsleague@gmail.com>
2020-11-16 18:59:06 -0500

    fix confusables dep

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export * as confusables from "https://raw.githubusercontent.com/gc/confusables/de723a0cc50d90d263b88ed638cbe96b63782443/src/index.ts";
+export * as confusables from "https://deno.land/x/confusables@1.0.0/mod.ts";
 export * from "https://x.nest.land/Discordeno@9.2.0/mod.ts";
 export * from "https://deno.land/std@0.67.0/fmt/colors.ts";
 export * from "https://deno.land/std@0.67.0/async/delay.ts";
