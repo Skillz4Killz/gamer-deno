db470b8bd0500b88e39993ac5a7aec723ee2d61d
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2020-11-17 21:38:00 -0500

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,10 +1,10 @@
 export * as confusables from "https://deno.land/x/confusables@1.0.0/mod.ts";
-export * from "https://x.nest.land/Discordeno@9.2.0/mod.ts";
+export * from "https://x.nest.land/Discordeno@9.4.0/mod.ts";
 export * from "https://deno.land/std@0.67.0/fmt/colors.ts";
 export * from "https://deno.land/std@0.67.0/async/delay.ts";
 export * from "https://deno.land/x/sabr@1.1.1/mod.ts";
 export * from "./cache.ts";
 
-import Client from "https://x.nest.land/Discordeno@9.2.0/src/module/client.ts";
+import Client from "https://x.nest.land/Discordeno@9.4.0/src/module/client.ts";
 
 export default Client;
