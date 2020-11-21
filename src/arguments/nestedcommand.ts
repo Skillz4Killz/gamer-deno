03607a892e9e7322fb6a5dcc762b9471b985e20a
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2020-11-21 23:44:20 +0000

    cleanup

--- src/arguments/nestedcommand.ts
+++ src/arguments/nestedcommand.ts
@@ -1,10 +1,11 @@
-import { botCache } from "../../cache.ts";
+import { botCache } from "../../deps.ts";
+
 botCache.arguments.set("nestedcommand", {
   name: "nestedcommand",
   execute: function (_argument, parameters) {
     let command = botCache.commands.get(parameters.join('\n'));
     if (command) return command;
-    
+
     for (const word of parameters) {
       const isCommand = command
         ? command.subcommands.get(word)
