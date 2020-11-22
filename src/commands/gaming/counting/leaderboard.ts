560523fe6050d437e80c0ec35c6c440f9ce06684
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2020-11-22 03:29:45 +0000

    Counting leaderboard

--- src/commands/gaming/counting/leaderboard.ts
+++ src/commands/gaming/counting/leaderboard.ts
@@ -1,3 +1,5 @@
+import { botCache, cache, sendMessage } from "../../../../deps.ts";
+import { db } from "../../../database/database.ts";
 import { createSubcommand } from "../../../utils/helpers.ts";
 
 createSubcommand("counting", {
@@ -9,26 +11,25 @@ createSubcommand("counting", {
     allowedUses: 2,
   },
   execute: async function (message, args, guild) {
-    // const settings = await countingDatabase.findOne(
-    //   { localOnly: false, guildID: message.guildID },
-    // );
-    // if (!settings) return botCache.helpers.reactError(message);
+    const leaderboards = (await db.counting.findMany({ localOnly: false }, true)).sort((a,b) => b.count - a.count);
+    const index = leaderboards.findIndex(lb => lb.guildID === message.guildID);
+    // Count not find this server
+    if (index < 0) return botCache.helpers.reactError(message);
 
-    // const test = countingDatabase.find(
-    //   { count: { $lte: settings.count }, localOnly: { $eq: false } },
-    // );
+    const current = leaderboards[index];
+    const top = leaderboards.slice(0, 9);
+    const above = index < 10 ? leaderboards.slice(0, 9) : leaderboards.slice(index - 9, index);
+    const below = index < 10 ? leaderboards.slice(10, 19) : leaderboards.slice(index + 1, index + 9);
+    
+    const list = [...top, ...above, ...below].map(data => {
+      const server = cache.guilds.get(data.guildID);
+      return `${server?.name || data.guildID} **[${data.channelID}]** \`${data.count}\` **(${Math.abs(data.count - current.count)})**`
+    });
 
-    // const [aboveTen, belowTen] = await Promise.all([
-    //   countingDatabase.find(
-    //     { localOnly: false, count: { $lte: settings.count } },
-    //   ).limit(10).sort({ count: -1 }),
-    //   countingDatabase.find(
-    //     { localOnly: false, count: { $gte: settings.count } },
-    //   ).limit(10).sort({ count: -1 }),
-    // ]);
+    const responses = botCache.helpers.chunkStrings(list);
 
-    // console.log("db check", aboveTen);
-    console.log("-------------------");
-    // console.log("db check", belowTen);
+    for (const response of responses) {
+      sendMessage(message.channelID, response);
+    }
   },
 });
