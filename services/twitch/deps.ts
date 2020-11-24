0ec3ad0814088de5f11ed7311923d94c928a8737
QuantumlyTangled <ethoarx@gmail.com>
2020-11-24 08:27:30 +0100

    basic post handling

--- services/twitch/deps.ts
+++ services/twitch/deps.ts
@@ -1,2 +1,3 @@
-export * from 'https://deno.land/x/oak/mod.ts';
 export * from 'https://deno.land/x/oak/helpers.ts';
+export * from 'https://deno.land/x/oak/mod.ts';
+export * from 'https://denopkg.com/chiefbiiko/hmac/mod.ts';
--- services/twitch/mod.ts
+++ services/twitch/mod.ts
@@ -1,4 +1,4 @@
-import { Application, Router } from './deps.ts';
+import { Application, Router, hmac } from './deps.ts';
 import { db } from '../../src/database/database.ts';
 
 const app = new Application();
@@ -15,18 +15,63 @@ router
     switch (mode) {
       case 'denied': {
         ctx.response.body = challenge ?? 'ok';
-        break;
+        return;
       }
       case 'unsubscribe':
       case 'subscribe': {
         ctx.response.body = challenge;
-        break;
+        return;
       }
       default: {
         ctx.response.status = 500;
         ctx.response.body = 'Fucking hell stop';
+        return;
       }
     }
+  })
+  .post('/:id', async (ctx) => {
+    ctx.response.headers.set('Content-Type', 'application/json');
+    ctx.response.status = 400;
+
+    if (!ctx.request.hasBody) {
+      ctx.response.body = {
+        reason: 'Malformed data received - request requires body'
+      }
+      return;
+    }
+
+    const xHubSignature = ctx.request.headers.get('x-hub-signature');
+    if (!xHubSignature) {
+      ctx.response.body = {
+        reason: 'Missing "x-hub-signature" header'
+      }
+      return;
+    }
+
+    const body = await ctx.request.body({ type: 'json' }).value;
+
+    const [algo, sig] = xHubSignature.split('=', 2);
+    // TODO: Add secret to config
+    const hash = hmac(algo, '', JSON.stringify(body), undefined, 'hex');
+
+    if (hash !== sig) {
+      ctx.response.status = 403;
+      ctx.response.body = {
+        reason: 'Invalid signature'
+      }
+      return;
+    }
+
+    const id = ctx.params.id;
+    if (!id) {
+      ctx.response.body = {
+        reason: 'Missing "id" parameter'
+      }
+      return;
+    }
+
+    const { data } = body;
+    const lengthStatus = data.length === 0;
   });
 
 app.use(router.routes());
