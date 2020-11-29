d4891dbb46434a13a9a44ff361b6ed78f52673b7
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2020-11-29 16:37:58 +0000

    MONKEY MONKEY MONKEY!

---
+++ src/commands/fun/randomnumber.ts
@@ -0,0 +1,19 @@
+import { botCache } from "../../../deps.ts";
+import { createCommand, sendResponse } from "../../utils/helpers.ts";
+
+createCommand({
+    name: "randomnumber",
+    aliases: ["rn"],
+    arguments: [
+        { name: "min", type: "number", defaultValue: 0 },
+        { name: "max", type: "number", defaultValue: 100 }
+    ],
+    execute: function (message, args: CommandArgs) {
+        sendResponse(message, botCache.helpers.cleanNumber(Math.floor(Math.random() * (args.max - args.min) + args.min)))
+    }
+})
+
+interface CommandArgs {
+    min: number;
+    max: number;
+}
