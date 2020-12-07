0c39abc49bad622b7207f4a379e812c5999e6204
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2020-12-07 17:10:11 +0000

    latest

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 export * as confusables from "https://deno.land/x/confusables@1.0.0/mod.ts";
-export { Image } from "https://deno.land/x/imagescript@1.1.10/mod.ts";
+export { Image } from "https://deno.land/x/imagescript@1.1.12/mod.ts";
 export * from "https://raw.githubusercontent.com/Skillz4Killz/Discordeno/next/mod.ts";
 export * from "https://deno.land/std@0.67.0/fmt/colors.ts";
 export * from "https://deno.land/std@0.67.0/async/delay.ts";
