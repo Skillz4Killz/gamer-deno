26ed1bb9f0bf3a46bb3eb72a93c6ae9200b674bd
Skillz4Killz <admin@ezlgg.com>
2020-12-13 20:48:46 -0500

    delete reminders

---
+++ src/commands/utility/reminders/delete.ts
@@ -0,0 +1,22 @@
+import { createSubcommand } from "../../../utils/helpers.ts";
+import { botCache } from "../../../../cache.ts";
+import { db } from "../../../database/database.ts";
+
+createSubcommand("remind", {
+  name: "delete",
+  cooldown: {
+    seconds: 120,
+    allowedUses: 2,
+  },
+  guildOnly: true,
+  arguments: [
+    { name: "id", type: "snowflake" },
+  ] as const,
+  execute: async (message, args) => {
+      const reminder = await db.reminders.get(args.id);
+      if (reminder?.memberID !== message.author.id) return botCache.helpers.reactError(message);
+
+      db.reminders.delete(args.id);
+      return botCache.helpers.reactSuccess(message);
+  },
+});
