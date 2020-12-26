8e72487430480f94c7e5f3220db32b494b77bf02
Skillz <gankstarsleague@gmail.com>
2020-12-25 21:28:15 -0500

    latest deps

--- deps.ts
+++ deps.ts
@@ -1,8 +1,7 @@
 export * as confusables from "https://deno.land/x/confusables@1.0.0/mod.ts";
 export { Image } from "https://deno.land/x/imagescript@1.1.12/mod.ts";
-export * from "https://raw.githubusercontent.com/discordeno/discordeno/f0f5ab72d980fc35951ecd7d701da3dcad6f93b9/mod.ts";
-export * from "https://deno.land/std@0.67.0/fmt/colors.ts";
-export * from "https://deno.land/std@0.67.0/async/delay.ts";
+export * from "https://raw.githubusercontent.com/discordeno/discordeno/next/mod.ts";
+export * from "https://deno.land/std@0.81.0/fmt/colors.ts";
 export * from "https://deno.land/x/sabr@1.1.4/mod.ts";
 export * from "./cache.ts";
 export * from "./fonts.ts";
