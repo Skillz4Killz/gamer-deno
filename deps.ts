f0b6463f75ef5f4c6f4a9088ecef5958b318cdbb
Skillz <gankstarsleague@gmail.com>
2021-01-12 21:47:15 -0500

    test new sabr

--- deps.ts
+++ deps.ts
@@ -2,6 +2,6 @@ export * as confusables from "https://deno.land/x/confusables@1.0.0/mod.ts";
 export { Image } from "https://deno.land/x/imagescript@1.1.14/mod.ts";
 export * from "https://raw.githubusercontent.com/discordeno/discordeno/master/mod.ts";
 export * from "https://deno.land/std@0.81.0/fmt/colors.ts";
-export * from "https://deno.land/x/sabr@1.1.4/mod.ts";
+export * from "https://raw.githubusercontent.com/Skillz4Killz/Sabr/master/mod.ts";
 export * from "./cache.ts";
 export * from "./fonts.ts";
