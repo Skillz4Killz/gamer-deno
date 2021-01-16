684ccee33d363ccce66694e79141af9d9b460676
Skillz <gankstarsleague@gmail.com>
2021-01-15 21:13:14 -0500

    fix is

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 export * as confusables from "https://deno.land/x/confusables@1.0.0/mod.ts";
-export { Image } from "https://deno.land/x/imagescript@1.1.14/mod.ts";
+export { Image } from "https://raw.githubusercontent.com/matmen/ImageScript/deno/mod.ts";
 export * from "https://raw.githubusercontent.com/discordeno/discordeno/master/mod.ts";
 export * from "https://deno.land/std@0.81.0/fmt/colors.ts";
 export * from "https://raw.githubusercontent.com/Skillz4Killz/Sabr/master/mod.ts";
