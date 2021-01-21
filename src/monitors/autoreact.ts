233f2a7353a68480e221d1fba7f37c7fbbb99f79
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-01-21 17:57:54 +0100

    review things

--- src/monitors/autoreact.ts
+++ src/monitors/autoreact.ts
@@ -3,6 +3,8 @@ import { botCache } from "../../deps.ts";
 import { getTime } from "../utils/helpers.ts";
 import { db } from "../database/database.ts";
 
+const SPECIAL_SERVER_IDS = new Set();
+
 botCache.monitors.set("autoreact", {
   name: "autoreact",
   botChannelPermissions: [
@@ -12,13 +14,15 @@ botCache.monitors.set("autoreact", {
     "USE_EXTERNAL_EMOJIS",
     "READ_MESSAGE_HISTORY",
   ],
+  ignoreBots: false,
   execute: async function (message) {
     if (!botCache.autoreactChannelIDs.has(message.channelID)) return;
+
+    if (message.author.bot && !SPECIAL_SERVER_IDS.has(message.guildID)) return;
 
     const settings = await db.autoreact.get(message.channelID);
     if (!settings) return;
 
-    if (message.author.bot && !botCache.vipGuildIDs.has(message.guildID)) return;
 
     await addReactions(message.channelID, message.id, settings.reactions).catch(console.log);
     console.log(
