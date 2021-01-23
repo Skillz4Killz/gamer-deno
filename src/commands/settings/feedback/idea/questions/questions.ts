5946234c73d0f8ef72fbf7ff84ef996630c63ecb
Skillz <gankstarsleague@gmail.com>
2021-01-23 11:12:15 -0500

    list questions

--- src/commands/settings/feedback/idea/questions/questions.ts
+++ src/commands/settings/feedback/idea/questions/questions.ts
@@ -1,5 +1,8 @@
+import { botCache } from "../../../../../../cache.ts";
+import { db } from "../../../../../database/database.ts";
 import { PermissionLevels } from "../../../../../types/commands.ts";
-import { createSubcommand } from "../../../../../utils/helpers.ts";
+import { Embed } from "../../../../../utils/Embed.ts";
+import { createSubcommand, sendEmbed } from "../../../../../utils/helpers.ts";
 
 createSubcommand("settings-feedback-idea", {
   name: "questions",
@@ -9,5 +12,15 @@ createSubcommand("settings-feedback-idea", {
     { name: "subcommand", type: "subcommand" },
   ],
   execute: async (message, args) => {
+    const settings = await db.guilds.get(message.guildID);
+    if (!settings) return botCache.helpers.reactError(message);
+
+    const embed = new Embed();
+
+    for (const data of [...settings.ideaQuestions, ...settings.bugsQuestions]) {
+      embed.addField(data.name, data.text);
+    }
+
+    await sendEmbed(message.channelID, embed);
   },
 });
