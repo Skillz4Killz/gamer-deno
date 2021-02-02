dae39ca921f30b4d947d60c944c707a7be41a1b0
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-02-02 21:51:59 +0100

    add todo

--- src/commands/gaming/currency/daily.ts
+++ src/commands/gaming/currency/daily.ts
@@ -1,9 +1,10 @@
 import { botCache } from "../../../../deps.ts";
 import { createCommand } from "../../../utils/helpers.ts";
 
+// TODO: add functionality
 createCommand({
   name: "daily",
   execute: async function (message) {
-    await botCache.helpers.reactError(message);
+    return botCache.helpers.reactError(message);
   },
 });
