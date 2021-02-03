dc1730f522424a3d2ed7dd0bc5e956825d0eb3a5
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-02-03 10:01:03 +0100

    Update remove.ts

--- src/commands/roles/reactions/remove.ts
+++ src/commands/roles/reactions/remove.ts
@@ -16,7 +16,7 @@ createSubcommand("roles-reactions", {
     { name: "name", type: "string", lowercase: true },
     { name: "emoji", type: "emoji" },
   ] as const,
-  execute: async function (message, args, guild) {
+  execute: async function (message, args) {
     const reactionRole = await db.reactionroles.findOne(
       { guildID: message.guildID, name: args.name },
     );
@@ -36,7 +36,7 @@ createSubcommand("roles-reactions", {
       reactionRole.channelID,
       reactionRole.messageID,
       emoji,
-    );
-    await botCache.helpers.reactSuccess(message);
+    ).catch(console.log);
+    return botCache.helpers.reactSuccess(message);
   },
 });
