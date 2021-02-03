49f640c6090b9b8e2844244b4e3160580b5ab5bf
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-02-03 09:56:54 +0100

    i

--- src/commands/roles/grouped/create.ts
+++ src/commands/roles/grouped/create.ts
@@ -1,19 +1,19 @@
 import { botCache } from "../../../../deps.ts";
-import { createSubcommand } from "../../../utils/helpers.ts";
-import { PermissionLevels } from "../../../types/commands.ts";
 import { db } from "../../../database/database.ts";
+import { PermissionLevels } from "../../../types/commands.ts";
+import { createSubcommand } from "../../../utils/helpers.ts";
 
 createSubcommand("roles-grouped", {
   name: "create",
   permissionLevels: [PermissionLevels.ADMIN],
   arguments: [
     { name: "name", type: "string", lowercase: true },
-    { name: "mainRole", type: "role", },
+    { name: "mainRole", type: "role" },
     { name: "roles", type: "...roles" },
   ] as const,
   vipServerOnly: true,
   guildOnly: true,
-  execute: async (message, args, guild) => {
+  execute: async (message, args) => {
     const exists = await db.groupedrolesets.findOne({
       name: args.name,
       guildID: message.guildID,
@@ -26,7 +26,7 @@ createSubcommand("roles-grouped", {
       name: args.name,
       roleIDs: args.roles.map((role) => role.id),
       guildID: message.guildID,
-      mainRoleID: args.mainRole.id
+      mainRoleID: args.mainRole.id,
     });
 
     return botCache.helpers.reactSuccess(message);
