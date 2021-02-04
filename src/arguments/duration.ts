0379faa96276797ffd0265cd0810cb7fac8f60e7
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-02-04 18:44:34 +0100

    Update duration.ts

--- src/arguments/duration.ts
+++ src/arguments/duration.ts
@@ -3,7 +3,7 @@ import { stringToMilliseconds } from "../utils/helpers.ts";
 
 botCache.arguments.set("duration", {
   name: "duration",
-  execute: async function (_argument, parameters, message) {
+  execute: async function (_argument, parameters) {
     const [time] = parameters;
     if (!time) return;
 
