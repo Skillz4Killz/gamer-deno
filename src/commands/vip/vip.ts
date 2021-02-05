0a70389e582e59130e7279270eb42ded3b13c83a
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-02-05 21:29:06 +0100

    Update vip.ts

--- src/commands/vip/vip.ts
+++ src/commands/vip/vip.ts
@@ -1,5 +1,5 @@
-import { botCache, cache } from "../../../deps.ts";
 import { configs } from "../../../configs.ts";
+import { botCache } from "../../../deps.ts";
 import { db } from "../../database/database.ts";
 import { createCommand } from "../../utils/helpers.ts";
 
@@ -19,32 +19,33 @@ createCommand({
     const member = message.member?.guilds.get(configs.supportServerID);
     if (!member) return botCache.helpers.reactError(message);
 
-    const allowedVIPServers =
-      member.roles.includes(configs.roleIDs.patreonRoleIDs.thirdTier)
-        ? 3
-        : member.roles.includes(configs.roleIDs.patreonRoleIDs.secondTier)
-        ? 2
-        : member.roles.includes(configs.roleIDs.patreonRoleIDs.firstTier)
-        ? 1
-        : 0;
+    const allowedVIPServers = configs.userIDs.botOwners.includes(
+      message.author.id
+    )
+      ? Infinity
+      : member.roles.includes(configs.roleIDs.patreonRoleIDs.thirdTier)
+      ? 3
+      : member.roles.includes(configs.roleIDs.patreonRoleIDs.secondTier)
+      ? 2
+      : member.roles.includes(configs.roleIDs.patreonRoleIDs.firstTier)
+      ? 1
+      : 0;
     if (!allowedVIPServers) return botCache.helpers.reactError(message, true);
 
     // Check if they have used all the vips.
     const settings = await db.users.get(message.author.id);
     if (
-      settings?.vipGuildIDs && settings.vipGuildIDs.length >= allowedVIPServers
+      settings?.vipGuildIDs &&
+      settings.vipGuildIDs.length >= allowedVIPServers
     ) {
       console.log(settings, allowedVIPServers);
       return botCache.helpers.reactError(message, true);
     }
 
-    await db.users.update(
-      message.author.id,
-      {
-        vipGuildIDs: [...(settings?.vipGuildIDs || []), message.guildID],
-        isVIP: true,
-      },
-    );
+    await db.users.update(message.author.id, {
+      vipGuildIDs: [...(settings?.vipGuildIDs || []), message.guildID],
+      isVIP: true,
+    });
     await db.guilds.update(message.guildID, { isVIP: true });
     botCache.vipGuildIDs.add(message.guildID);
     botCache.vipUserIDs.add(message.author.id);
