851e720984a0bccd52afbb56cd2408940395f3a0
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-02-09 18:45:10 +0100

    add db to configs

--- services/twitter/configs.example.ts
+++ services/twitter/configs.example.ts
@@ -1,10 +1,6 @@
-import { fromFileUrl } from "./deps.ts";
+import { fromFileUrl } from "../../deps.ts";
 
 export const configs = {
-  // Your Twitch Client ID here. (https://dev.twitch.tv/docs/v5#getting-a-client-id)
-  clientID: "",
-  // Your Twitch Client Secret here. (You can get it in your Dashboard https://dev.twitch.tv/console/apps)
-  clientSecret: "",
   database: {
     directoryPath: `${
       fromFileUrl(
