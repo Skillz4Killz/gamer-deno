8bed0e1e22438f5ecc5547b9ae2efd36195c8c17
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-02-09 15:53:45 -0500

    Update deps.ts

--- services/twitch/deps.ts
+++ services/twitch/deps.ts
@@ -1,2 +1,2 @@
-export { executeWebhook } from "https://raw.githubusercontent.com/discordeno/discordeno/next/src/handlers/webhook.ts";
+export * from "https://raw.githubusercontent.com/discordeno/discordeno/master/mod.ts";
 export * from "https://raw.githubusercontent.com/Skillz4Killz/Sabr/master/mod.ts";
