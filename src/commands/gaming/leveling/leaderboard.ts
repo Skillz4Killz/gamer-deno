f3df69bb0cc5541062041b97a390df8be2419bf4
dys0n-V-nT <59572651+dys0n-V-nT@users.noreply.github.com>
2021-02-13 14:54:31 +0100

    now

--- src/commands/gaming/leveling/leaderboard.ts
+++ src/commands/gaming/leveling/leaderboard.ts
@@ -1,5 +1,5 @@
-import { botCache, cache } from "../../../../deps.ts";
 import { createCommand } from "../../../utils/helpers.ts";
+// import { botCache, cache } from "../../../../deps.ts";
 
 createCommand({
   name: "leaderboard",
@@ -16,19 +16,25 @@ createCommand({
     { name: "member", type: "member", required: false },
   ] as const,
   execute: async function (message, args) {
-    if (!args.member) args.member = cache.members.get(message.author.id)!;
-    if (!args.member) return botCache.helpers.reactError(message);
 
-    const buffer = await botCache.helpers.makeLocalCanvas(
-      message,
-      args.member,
-    );
-    if (!buffer) return botCache.helpers.reactError(message);
-
-    const embed = botCache.helpers.authorEmbed(message).attachFile(
-      buffer,
-      "profile.jpg",
-    );
+   // Remove that after move to mongodb
     return message.reply("This command is currently disabled due to a database problem.");
+
+   //  if (!args.member) args.member = cache.members.get(message.author.id)!;
+   //  if (!args.member) return botCache.helpers.reactError(message);
+   //
+   //  const buffer = await botCache.helpers.makeLocalCanvas(
+   //    message,
+   //    args.member,
+   //  );
+   //  if (!buffer) return botCache.helpers.reactError(message);
+   //  
+   //  return message.reply("This command is currently disabled due to a database problem.");
+
+   //const embed = botCache.helpers.authorEmbed(message).attachFile(
+   // buffer,
+   // "profile.jpg",
+   //);
+   //return message.send({ embed, file: embed.embedFile });
   },
 });
