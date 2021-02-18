b5f2aec61ffca54e73192ab5d208e5551494c8ca
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-02-18 20:55:33 +0100

    nickname update add mod check

--- src/events/nickname.ts
+++ src/events/nickname.ts
@@ -1,4 +1,4 @@
-import { botCache, guildIconURL } from "../../deps.ts";
+import { botCache, getAuditLogs, guildIconURL, rawAvatarURL } from "../../deps.ts";
 import { db } from "../database/database.ts";
 import { Embed } from "../utils/Embed.ts";
 import { sendEmbed } from "../utils/helpers.ts";
@@ -43,6 +43,26 @@ botCache.eventHandlers.nicknameUpdate = async function (guild, member, nick, old
   if (logs.memberNickPublic) {
     await sendEmbed(logs.publicChannelID, embed);
   }
+
+  // WAIT FEW SECONDS TO ALLOW AUDIT LOGS AVAILABLE
+  const auditlogs = await getAuditLogs(guild.id, {
+    action_type: "MEMBER_UPDATE",
+  }).catch(console.log);
+
+  // IF A LOG WAS NOT FOUND, POST NORMAL EMBED
+  const relevant = auditlogs?.audit_log_entries.find((e: any) => e.target_id === member.id);
+  if (!relevant) {
+    return sendEmbed(logs.memberNickChannelID, embed);
+  }
+
+  const mod = auditlogs.users.find((u: any) => u.id === relevant.user_id);
+  if (mod) {
+    embed.setAuthor(
+      `${mod.username}#${mod.discriminator} (${mod.id})`,
+      rawAvatarURL(mod.id, mod.discriminator, mod.avatar)
+    );
+  }
+
   // SEND PRIVATE NICK LOG
   return sendEmbed(logs.memberNickChannelID, embed);
 };
