d745cf7cf4b73ed3729a46570875273813adb322
dys0n-V-nT <59572651+dys0n-V-nT@users.noreply.github.com>
2021-02-25 00:57:03 +0100

    add a good command

---
+++ src/commands/roles/membercount.ts
@@ -0,0 +1,48 @@
+import { botCache, cache, fetchMembers } from "../../../deps.ts";
+import { PermissionLevels } from "../../types/commands.ts";
+import { createSubcommand } from "../../utils/helpers.ts";
+import { Embed } from "../../utils/Embed.ts";
+import { translate } from "../../utils/i18next.ts";
+
+createSubcommand("roles", {
+  name: "membercount",
+  aliases: ["mc"],
+  permissionLevels: [PermissionLevels.ADMIN, PermissionLevels.MODERATOR],
+  botChannelPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+  guildOnly: true,
+  vipServerOnly: true,
+  arguments: [{ name: "role", type: "role" }] as const,
+  execute: async function (message, args, guild) {
+    if (!guild) return;
+
+    const guildMembersCached = cache.members.filter((m) => m.guilds.has(message.guildID));
+    if (guildMembersCached.size !== guild.memberCount) {
+      await fetchMembers(guild);
+    }
+
+    let botCount = 0;
+    let memberCount = 0;
+
+    for (const member of cache.members.values()) {
+      if (!member.guilds.has(message.guildID)) continue;
+
+      // If not everyone role and member doesnt have this role skip
+      if (message.guildID !== args.role.id && !member.guilds.get(message.guildID)?.roles.includes(args.role.id)) {
+        continue;
+      }
+
+      if (member.bot) botCount++;
+      else memberCount++;
+    }
+    const color = `#${args.role.color.toString(16).toUpperCase()}`;
+    const embed = new Embed()
+      .setColor(color)
+      .setTitle(args.role.name)
+      .addField(translate(message.guildID, "strings:ROLE_USERCOUNT"), memberCount.toLocaleString(), false)
+      .addField(translate(message.guildID, "strings:ROLE_BOTCOUNT"), botCount.toLocaleString(), false)
+      .setFooter(translate(message.guildID, "strings:CREATED_AT"))
+      .setTimestamp(botCache.helpers.snowflakeToTimestamp(args.role.id));
+
+    return message.send({ embed });
+  },
+});
