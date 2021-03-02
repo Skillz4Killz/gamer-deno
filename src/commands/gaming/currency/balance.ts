d3d487d3af39db39e10e511dde38048ab27843d0
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-03-02 22:20:29 +0100

    allow balance from others?

--- src/commands/gaming/currency/balance.ts
+++ src/commands/gaming/currency/balance.ts
@@ -5,17 +5,18 @@ import { createCommand } from "../../../utils/helpers.ts";
 createCommand({
   name: "balance",
   aliases: ["bal", "coins"],
+  arguments: [{ type: "snowflake", name: "user", required: false }],
   cooldown: {
     seconds: 30,
     allowedUses: 6,
   },
-  execute: async function (message) {
-    const settings = await db.users.get(message.author.id);
+  execute: async function (message, args) {
+    const settings = await db.users.get(args.user ?? message.author.id);
     if (!settings) return botCache.helpers.reactError(message);
 
     let amount = settings.coins || 0;
 
-    const marriage = await db.marriages.get(message.author.id);
+    const marriage = await db.marriages.get(args.user ?? message.author.id);
     if (marriage && marriage.accepted) {
       const spouse = await db.users.get(marriage?.spouseID);
       if (spouse) amount += spouse.coins;
