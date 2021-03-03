122e11df65b21c1bd59174f1e0a68d9e3f612e17
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-03-03 18:16:02 +0100

    bal better arg

--- src/commands/gaming/currency/balance.ts
+++ src/commands/gaming/currency/balance.ts
@@ -5,18 +5,18 @@ import { createCommand } from "../../../utils/helpers.ts";
 createCommand({
   name: "balance",
   aliases: ["bal", "coins"],
-  arguments: [{ type: "snowflake", name: "user", required: false }],
+  arguments: [{ type: "member", name: "member", required: false }],
   cooldown: {
     seconds: 30,
     allowedUses: 6,
   },
   execute: async function (message, args) {
-    const settings = await db.users.get(args.user ?? message.author.id);
+    const settings = await db.users.get(args.member.id ?? message.author.id);
     if (!settings) return botCache.helpers.reactError(message);
 
     let amount = settings.coins || 0;
 
-    const marriage = await db.marriages.get(args.user ?? message.author.id);
+    const marriage = await db.marriages.get(args.member.id ?? message.author.id);
     if (marriage && marriage.accepted) {
       const spouse = await db.users.get(marriage?.spouseID);
       if (spouse) amount += spouse.coins;
