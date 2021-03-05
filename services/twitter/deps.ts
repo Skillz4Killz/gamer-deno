e77d641ada806d281b3661e10cf460c74f4f5264
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-03-05 17:25:17 +0100

    fix imports

--- services/twitter/deps.ts
+++ services/twitter/deps.ts
@@ -1,2 +1,2 @@
-export * from "https://raw.githubusercontent.com/discordeno/discordeno/master/mod.ts";
+export * from "https://raw.githubusercontent.com/discordeno/discordeno/10.4.0/mod.ts";
 export * from "https://raw.githubusercontent.com/Skillz4Killz/Sabr/master/mod.ts";
