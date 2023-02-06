53f81bb317958ce7a3e95378b10281893dcb30e6
Skillz4Killz <drzishan12@gmail.com>
2023-02-06 02:54:08 +0000

    fix: base bot

---
+++ src/bot.ts
@@ -0,0 +1,14 @@
+import { Bot, createBot } from "@discordeno/bot";
+import { configs } from "../old/configs.example";
+
+const Gamer: GamerBot = {
+  discord: createBot({
+    token: configs.token,
+    events: {},
+  }),
+};
+
+export interface GamerBot {
+  /** The bot on discord platform */
+  discord: Bot;
+}
