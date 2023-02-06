a229d83101eebad9fa30915ddcca832f8ea1d648
Skillz4Killz <drzishan12@gmail.com>
2023-02-06 18:20:27 +0000

    fix: more configs

--- src/configs.ts
+++ src/configs.ts
@@ -1,5 +1,9 @@
 // TODO: dotenv - every config from .env file should be brought into here and typechecked.
 export const configs = {
+    /** Config settings for the bot itself. */
+    bot: {
+        name: "Gamer",
+    },
     /** The command prefix for message based commands. */
     prefix: "",
     /** Configs related to specific platforms. */
