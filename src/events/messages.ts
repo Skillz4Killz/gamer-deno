09c3e86e2dc10469bd32660783868b3af0c8b6b2
Skillz4Killz <drzishan12@gmail.com>
2023-02-07 05:21:33 +0000

    chore: cleanup

--- src/events/messages.ts
+++ src/events/messages.ts
@@ -1,16 +1,10 @@
 import { Camelize, DiscordMessage } from "@discordeno/bot";
 import { Message } from "guilded.js/types/index.js";
 import { GamerMessage } from "../base/GamerMessage.js";
-import { Gamer } from "../bot.js";
 import { handlePossibleCommand } from "./helpers/commands.js";
 
 export async function messageCreate(payload: Camelize<DiscordMessage> | Message) {
     const message = new GamerMessage(payload);
-    if (message.isOnDiscord) {
-        Gamer.loggers.discord.info(`[Event] MessageCreate: ${message.content || message.embeds.length}`);
-    } else {
-        Gamer.loggers.guilded.info(`[Event] MessageCreate: ${message.content}`)
-    }
 
     // Run all the handling for a new message
     await Promise.all([
--- src/startup.ts
+++ src/startup.ts
@@ -1,4 +1,4 @@
-import { loadCommands, makeInteractionCommands } from "./commands/index.js";
+import { loadCommands } from "./commands/index.js";
 import { Gamer } from "./bot.js";
 import { configs } from "./configs.js";
 import { setEventsOnGuilded } from "./events/guilded.js";
@@ -10,12 +10,13 @@ export async function startup() {
     // TODO: prisma - Load database values into cache
 
     if (configs.platforms.discord.token) {
-        if (configs.devServerId) {
-            const interactionCommands = makeInteractionCommands();
-            Gamer.loggers.discord.info(`[Startup] Updating interaction commands.`)
-            await Gamer.discord.rest.upsertGuildApplicationCommands(configs.devServerId, interactionCommands)
-            Gamer.loggers.discord.info(`[Startup] Updated interaction commands.`)
-        }
+        // TODO: interactions - re-enable this when debuggable
+        // if (configs.devServerId) {
+        //     const interactionCommands = makeInteractionCommands();
+        //     Gamer.loggers.discord.info(`[Startup] Updating interaction commands.`)
+        //     await Gamer.discord.rest.upsertGuildApplicationCommands(configs.devServerId, interactionCommands)
+        //     Gamer.loggers.discord.info(`[Startup] Updated interaction commands.`)
+        // }
 
         Gamer.loggers.discord.info(`[Startup] Starting Discord bot.`)
         await Gamer.discord.start();
