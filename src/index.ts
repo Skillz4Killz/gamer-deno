86f671816bec8145dc3430a676984b51b317bd5b
Skillz4Killz <drzishan12@gmail.com>
2023-02-07 15:03:09 +0000

    fix: interaction setup bug

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
+import { Gamer } from "./bot.js";
 import { startup } from "./startup.js";
 
 // TODO: eslint - Setup same way as dd eslint
-startup()
+startup();
+
+process.on("unhandledRejection", (reason, promise) => {
+    if (typeof reason === "string") {
+        return Gamer.loggers.discord.error(reason, promise);
+    }
+
+    Gamer.loggers.discord.error(JSON.stringify(reason, undefined, 2))
+});
