18eb4f6380f301af9341c64216ebe02086688e08
Skillz <skillz@discord.gg/ddeno>
2023-02-07 21:28:05 -0600

    fix: support for button clicks and modals

--- src/events/interactions.ts
+++ src/events/interactions.ts
@@ -1,27 +1,55 @@
-import { ApplicationCommandOptionTypes, Camelize, DiscordInteraction } from "@discordeno/bot";
+import { ApplicationCommandOptionTypes, Camelize, DiscordInteraction, InteractionTypes, MessageComponentTypes } from "@discordeno/bot";
 import { GamerMessage } from "../base/GamerMessage.js";
 import { Gamer } from "../bot.js";
 
 export async function interactionCreate(payload: Camelize<DiscordInteraction>) {
     if (!payload.data) return Gamer.loggers.discord.debug("Interaction arrived without a data payload", payload);
 
-    Gamer.loggers.discord.info(`[Command] Interaction ${payload.data?.name} seen.`);
-    const message = new GamerMessage(payload);
+    payload.user = payload.user ?? payload.member!.user;
 
-    const command = Gamer.commands.get(payload.data.name) ?? Gamer.commands.find((cmd) => cmd.aliases.includes(payload.data!.name));
-    if (!command) return Gamer.loggers.discord.warn(`[Command] Interaction without a valid command.`, payload);
+    if (payload.type === InteractionTypes.ApplicationCommand) {
+        Gamer.loggers.discord.info(`[Command] Interaction ${payload.data?.name} seen.`);
+        const message = new GamerMessage(payload);
 
-    // TODO: args - convert interaction options to args
-    // console.log(JSON.stringify(payload, undefined, 2));
+        const command = Gamer.commands.get(payload.data.name) ?? Gamer.commands.find((cmd) => cmd.aliases.includes(payload.data!.name));
+        if (!command) return Gamer.loggers.discord.warn(`[Command] Interaction without a valid command.`, payload);
 
-    const args: Record<string, any> = {};
-    for (const option of payload.data.options ?? []) {
-        if (option.type === ApplicationCommandOptionTypes.User) {
-            args[option.name] = payload.data.resolved?.users?.[option.value as string];
-            continue;
+        // TODO: args - convert interaction options to args
+        // console.log(JSON.stringify(payload, undefined, 2));
+
+        const args: Record<string, any> = {};
+        for (const option of payload.data.options ?? []) {
+            if (option.type === ApplicationCommandOptionTypes.User) {
+                args[option.name] = payload.data.resolved?.users?.[option.value as string];
+                continue;
+            }
+
+            args[option.name] = option.value;
+        }
+        return await command.execute(message, args);
+    }
+
+    if (payload.type === InteractionTypes.MessageComponent) {
+        if (payload.data.componentType === MessageComponentTypes.Button) {
+            Gamer.loggers.discord.info(
+                `[Button] The ${payload.data.customId} button was clicked in Guild: ${payload.data.guildId} by ${payload.user.id}.`,
+            );
+
+            await Promise.allSettled([
+                // Button handlers can go here
+            ]).catch(console.log);
         }
 
-        args[option.name] = option.value;
+        return;
     }
-    command.execute(message, args);
+
+    if (payload.type === InteractionTypes.ModalSubmit) {
+        Gamer.loggers.discord.info(
+            `[Modal] The ${payload.data?.customId || "UNKNOWN"} modal was submitted in Guild: ${payload.guildId} by ${payload.user.id}.`,
+        );
+        
+        return await Promise.allSettled([
+            // Modal handlers can go here
+        ]).catch(console.log);
+    }
 }
