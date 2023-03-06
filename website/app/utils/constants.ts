edb624c94423440d717a56e83f07e90fd2f10415
Skillz <gankstarsleague@gmail.com>
2023-03-06 11:07:49 -0600

    fix: remove site

--- website/app/utils/constants.ts
+++
@@ -1,3 +0,0 @@
-export const SUPPORT_SERVER = "https://discord.gg/J4NqJ72";
-export const BOT_INVITE =
-    "https://discordapp.com/oauth2/authorize?client_id=635877338462027783&scope=bot+applications.commands&permissions=2111302911";
